7585f1724fc2c74d66fe16afe00fe6189b1de02f
sterio0o <ilyanaumov203@gmail.com>
2026-07-19 22:13:33 +0400

    Добавил Kafka в Analyzer Service

--- analyzer-service/src/main/java/dev/github/sterio0o/analyzerservice/service/StartupRunner.java
+++ analyzer-service/src/main/java/dev/github/sterio0o/analyzerservice/service/StartupRunner.java
@@ -1,11 +1,14 @@
 package dev.github.sterio0o.analyzerservice.service;
 
+import dev.github.sterio0o.analyzerservice.kafka.KafkaProducer;
 import dev.github.sterio0o.analyzerservice.model.Source;
 import dev.github.sterio0o.analyzerservice.model.User;
 import dev.github.sterio0o.analyzerservice.repository.UserRepository;
+import dev.github.sterio0o.common.event.SuccessfulDataAnalysisEvent;
 import dev.github.sterio0o.common.util.AdapterType;
 import dev.github.sterio0o.common.util.AggregateContent;
 import dev.github.sterio0o.common.util.ProcessedContent;
+import dev.github.sterio0o.common.util.Report;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
@@ -22,6 +25,7 @@ public class StartupRunner implements CommandLineRunner {
     private final ContentProcessingService contentProcessingService;
     private final CollectionSchedulerService collectionSchedulerService;
     private final AiService aiService;
+    private final KafkaProducer kafkaProducer;
 
     @Override
     public void run(String... args) throws Exception {
@@ -42,10 +46,14 @@ public class StartupRunner implements CommandLineRunner {
                     // Обрабатывает контент
                     List<ProcessedContent> contents = contentProcessingService.processedContent(user.getId(), types);
 
-                    aiService.generateReport(user.getId(), contents, user.getKeywords());
+                    Report report = aiService.generateReport(user.getId(), contents, user.getKeywords());
 
-                    // Дописать отправку события через Kafka в Delivery Service
+                    SuccessfulDataAnalysisEvent event = new SuccessfulDataAnalysisEvent(
+                            user.getId(),
+                            report
+                    );
 
+                    kafkaProducer.sendEvent(event);
                     log.info("Runnable успешно выполнен и событие отправлено в Kafka, user: {}", user.getId());
                 } catch (Exception e) {
                     log.info("Ошибка в Runnable user: {}", user.getId());
